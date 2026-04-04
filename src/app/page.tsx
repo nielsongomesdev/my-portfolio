fe93dd67c7d3e12f70bb62a54e6a8bc3fea451d7
Nielson Gomes <nielsongomes16@gmail.com>
2026-04-04 09:44:10 -0300

    feat: adiciona sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Hero } from "@/components/Hero";
+import { Sidebar } from "@/components/Sidebar";
 
 export default function Home() {
   return (
     <div className="flex flex-col">
       <Hero />
+      <Sidebar />
     </div>
   );
 }
